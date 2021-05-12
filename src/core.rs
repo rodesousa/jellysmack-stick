d6c0567680bd991f976544c3e4db559b44c79030
rodesousa <dessroberto@gmail.com>
2021-05-12 05:25:03 +0200

    it's done
    Signed-off-by: rodesousa <dessroberto@gmail.com>

--- src/core.rs
+++ src/core.rs
@@ -2,10 +2,12 @@ use crate::error::*;
 use std::io;
 
 type Line = Vec<char>;
-type Pipe = Vec<(usize, usize)>;
+
+// Saves grid ids
+type Pipes = Vec<(usize, usize)>;
 
 // Reads the file and return head/tail pair and pipes
-pub fn engine() -> Result<(Line, Line, Pipe), StickError> {
+pub fn engine() -> Result<(Line, Line, Pipes), StickError> {
     let mut input_line = String::new();
     match io::stdin().read_line(&mut input_line) {
         Err(_) => Err(StickError::InputError),
@@ -18,13 +20,14 @@ pub fn engine() -> Result<(Line, Line, Pipe), StickError> {
                 exit(StickError::ConstraintError)
             }
 
-            let (head, tail, result) = function_name(width, height)?;
+            let (head, tail, result) = find_pipes(width, height)?;
             Ok((head, tail, result))
         }
     }
 }
 
-pub fn solution(head: Line, mut tail: Line, result: Pipe) -> (Line, Line) {
+// Swaps tail to find pairs
+pub fn solution(head: Line, mut tail: Line, result: Pipes) -> (Line, Line) {
     for i in result.iter().rev() {
         tail.swap(i.0, i.1)
     }
@@ -39,20 +42,8 @@ fn init(input: &str) -> Result<i32, StickError> {
     }
 }
 
-fn read_lines(width: i32) -> Result<String, StickError> {
-    let mut input_line = String::new();
-    io::stdin().read_line(&mut input_line).unwrap();
-    let lines = input_line.trim().to_string();
-
-    if width as usize == lines.len() {
-        Ok(lines)
-    } else {
-        Err(StickError::WidthMissMatchError(lines.len(), width))
-    }
-}
-
-fn function_name(width: i32, height: i32) -> Result<(Line, Line, Pipe), StickError> {
-    let mut result: Pipe = Vec::new();
+fn find_pipes(width: i32, height: i32) -> Result<(Line, Line, Pipes), StickError> {
+    let mut result: Pipes = Vec::new();
     let mut head: Line = Vec::new();
     let mut tail: Line = Vec::new();
     let mut before_pipe;
@@ -97,6 +88,28 @@ fn function_name(width: i32, height: i32) -> Result<(Line, Line, Pipe), StickErr
     }
 }
 
+fn read_pair() -> Line {
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    input_line
+        .trim()
+        .split(" ")
+        .filter(|x| !x.is_empty())
+        .map(|x| x.chars().nth(0).unwrap())
+        .collect::<Line>()
+}
+
+fn read_lines(width: i32) -> Result<String, StickError> {
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let lines = input_line.trim().to_string();
+    if width as usize == lines.len() {
+        Ok(lines)
+    } else {
+        Err(StickError::WidthMissMatchError(lines.len(), width))
+    }
+}
+
 fn is_border(len: usize, id: usize) -> bool {
     id + 1 == len || id == 0
 }
@@ -116,15 +129,4 @@ fn pair_contraints(head: &Line, tail: &Line) -> Result<(), StickError> {
     }
 
     Ok(())
-}
-
-fn read_pair() -> Line {
-    let mut input_line = String::new();
-    io::stdin().read_line(&mut input_line).unwrap();
-    input_line
-        .trim()
-        .split(" ")
-        .filter(|x| !x.is_empty())
-        .map(|x| x.chars().nth(0).unwrap())
-        .collect::<Line>()
 }
